eb23d9e395a5e023c6bf2b2914c4632af4db55b5
Mohammed Rizwan <mrizwan9022@gmail.com>
2019-02-17 16:47:12 +0530

    change .env configration

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -3,4 +3,16 @@
 @section('content')
 
 <h1>Home Page</h1>
+
+<div class="row class mt-4">
+<p class="bg-dark text-light p-3 col-md-6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum eius consequatur amet. Libero cum consequuntur optio, iusto, ea quod vero eum quidem totam saepe ab laboriosam praesentium rem voluptatibus hic.
+Magnam, architecto nobis. Consequatur aspernatur placeat voluptates autem quidem animi quae nulla delectus veniam vel. Impedit harum officiis, et autem iste ab reiciendis, placeat accusamus totam modi ipsum obcaecati voluptas.
+Delectus omnis nisi, minus quibusdam pariatur, aspernatur perspiciatis nobis unde maxime deleniti, sed nesciunt velit tenetur officia! Cum quidem dolore repellat. Dignissimos autem corrupti ducimus rem temporibus repellendus iste ab.</p>
+
+<p class="bg-danger text-light p-3 col-md-6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum eius consequatur amet. Libero cum consequuntur optio, iusto, ea quod vero eum quidem totam saepe ab laboriosam praesentium rem voluptatibus hic.
+Magnam, architecto nobis. Consequatur aspernatur placeat voluptates autem quidem animi quae nulla delectus veniam vel. Impedit harum officiis, et autem iste ab reiciendis, placeat accusamus totam modi ipsum obcaecati voluptas.
+Quis, sit, nam perferendis fugit temporibus adipisci repellat rerum earum error eligendi quibusdam consequuntur, et sed reiciendis debitis omnis eos. Excepturi beatae et quas ipsa autem molestiae nemo. Tempora, voluptatum!
+Delectus omnis nisi, minus quibusdam pariatur, aspernatur perspiciatis nobis unde maxime deleniti, sed nesciunt velit tenetur officia! Cum quidem dolore repellat. Dignissimos autem corrupti ducimus rem temporibus repellendus iste ab.</p>
+</div>
 @endsection
+
